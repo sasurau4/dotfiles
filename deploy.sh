c8f66657b7c8dcb58b6dd9efa259925ff92f903d
Daiki Ihara <ihara@Daiki-Ihara.local>
2018-01-30 16:36:47 +0900

    rename install.sh -> deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DOT_DIRECTORY="${HOME}/dotfiles"
+cd ${DOT_DIRECTORY}
+
+for f in .??*
+do
+  # 無視したいファイルやディレクトリはこんな風に追加してね
+  [[ ${f} = ".git" ]] && continue
+  [[ ${f} = ".gitignore" ]] && continue
+  ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
+echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
