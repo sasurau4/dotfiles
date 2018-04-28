1f3a92a91a7d1d920bea53cb1e1c09ded1981955
Daiki Ihara <sasurau4@gmail.com>
2018-04-28 23:06:29 +0900

    update deploy & init scripts

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,16 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 cd ${DOT_DIRECTORY}
 
 echo $(tput setaf 4)Cloning submodules start.$(tput sgr0)
-git submodule init
-git submodule update --depth= 1
+git submodule update --init --recursive --depth 1
 echo $(tput setaf 4)Cloning submodules done! ✔︎$(tput sgr0)
 
 echo $(tput setaf 4)Create or update symlinnks for dotfiles$(tput sgr0)
 for f in .??*
 do
   # 無視したいファイルやディレクトリはこんな風に追加してね
-  [[ ${f} = ".git" ]] && continue
-  [[ ${f} = ".gitignore" ]] && continue
+  [ ${f} = ".git" ] && continue
+  [ ${f} = ".gitignore" ] && continue
+  [ ${f} = ".gitmodules" ] && continue
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 echo $(tput setaf 4)Create or update symlinnks for dotfiles done! ✔︎$(tput sgr0)
--- init.sh
+++ init.sh
@@ -5,11 +5,9 @@ echo $(tput setaf 2)initialize dotfiles start!.$(tput sgr0)
 DOT_DIRECTORY="${HOME}/dotfiles"
 cd ${DOT_DIRECTORY}
 
-git submodule init
-git submodule update --depth= 1
+echo $(tput setaf 4)Cloning submodules start! ✔︎$(tput sgr0)
+git submodule update --init --recursive --depth 1
 echo $(tput setaf 4)Cloning submodules done! ✔︎$(tput sgr0)
-
-
 
 echo $(tput setaf 4)install nerd fonts.$(tput sgr0)
 ./.vim/icons/nerd-fonts/install.sh
