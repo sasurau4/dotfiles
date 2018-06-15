1db3d5d4fa52d6ccb16be60bcb7f2785c8adbbac
Daiki Ihara <sasurau4@gmail.com>
2018-06-16 00:37:21 +0900

    modify script

--- addvimplugin.sh
+++ addvimplugin.sh
@@ -3,5 +3,5 @@
 # install vim plugin as submodule
 DOT_DIRECTORY="${HOME}/dotfiles"
 cd ${DOT_DIRECTORY}/.vim/pack/mypackage/start/
-git submodule add --depth 1 -- $1
+git submodule add $1
 cd ${DOT_DIRECTORY}
