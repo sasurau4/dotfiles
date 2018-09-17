d2e96c9ed962b837762c371749af384d7192cfc3
Daiki Ihara <sasurau4@gmail.com>
2018-09-17 15:22:52 +0900

    update scripts

--- scripts/update-node-with-nvm.sh
+++ scripts/update-node-with-nvm.sh
@@ -5,10 +5,5 @@ set -eu
 DOT_DIRECTORY="${HOME}/dotfiles"
 cd ${DOT_DIRECTORY}
 
-# you run before nvm ls-remote and check latest verion.
-# null check
-$1:?
-nvm install $1
-
 npm i -g yarn
 yarn global add javascript-typescript-langserver
