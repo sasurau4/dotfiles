bcf5bb79e4e0d0e875ea489c90da5953beafde22
Daiki Ihara <sasurau4@gmail.com>
2021-05-17 23:21:22 +0900

    update

--- scripts/invert-scroll-for-ubuntu.sh
+++ scripts/invert-scroll-for-ubuntu.sh
@@ -5,4 +5,5 @@ set -eu
 DOT_DIRECTORY="${HOME}/dotfiles"
 cd ${DOT_DIRECTORY}
 
+# see https://askubuntu.com/questions/1029128/inverted-horizontal-scrolling-ubuntu-18-04
 xinput set-prop 10 332 -74, -74
