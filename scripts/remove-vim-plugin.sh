01bce67aec9c976fbff84c2b7b16e7258c204579
Daiki Ihara <sasurau4@gmail.com>
2021-09-06 17:58:14 +0900

    Chore

--- scripts/remove-vim-plugin.sh
+++ scripts/remove-vim-plugin.sh
@@ -3,5 +3,7 @@
 set -eu
 DOT_DIRECTORY="${HOME}/dotfiles"
 VIM_PLUGIN_DIRECTORY="${DOT_DIRECTORY}/.vim/pack/mypackage/start/$1"
+GIT_MODULE_DIRECTORY="${DOT_DIRECTORY}/.git/modules/.vim/pack/mypackage/start/$1"
 git submodule deinit $VIM_PLUGIN_DIRECTORY
 git rm $VIM_PLUGIN_DIRECTORY
+rm -rf $GIT_MODULE_DIRECTORY
