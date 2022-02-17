5915d6e373dc9e7e0cf568393f49ce7d86cbac98
Daiki Ihara <sasurau4@gmail.com>
2022-02-18 00:34:10 +0900

    Add less option

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -37,6 +37,9 @@ set -x PATH $ANDROID_HOME/tools $PATH
 set -x PATH $ANDROID_HOME/tools/bin $PATH
 set -x PATH $ANDROID_HOME/platform-tools $PATH
 set -x PATH $ANDROID_HOME/ndk-bundle $PATH
+# https://qiita.com/delphinus/items/b04752bb5b64e6cc4ea9#%E7%92%B0%E5%A2%83%E5%A4%89%E6%95%B0-pager
+set -x LESS '-i -M -R -W'
+set -x PAGER less
 
 # fisher
 # if not functions -q fisher
