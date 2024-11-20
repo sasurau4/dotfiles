13a41047cf70cf3319454fc26f1a8a96bed6ade8
Daiki Ihara <sasurau4@gmail.com>
2024-11-20 20:28:41 +0900

    fix

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -101,7 +101,5 @@ case Darwin
   # Lock the screen (when going AFK)
   alias afk '/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
 case Linux
-  alias clang 'clang++-18'
-  alias lld-link 'lld-link-18'
 end
 
